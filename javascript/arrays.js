ffeff0421423e3ba2442433116fb38c96cd6c7b2
Nick <Nickjv2000@hotmail.nl>
2021-02-17 04:11:52 +0100

    Adding images / renaming file
    Added 12 images in the image directory and added it to what was previously called images.js, which has now been renamed to arrays.js and has gotten the arrays images and names. Including the names of the images.

---
+++ javascript/arrays.js
@@ -0,0 +1,29 @@
+var images = [
+	"among.jpg"
+	"apex.jpg"
+	"csgo.jpg"
+	"fortnite.jpg"
+	"GTA.jpg"
+	"LoL.jpg"
+	"minecraft.jpg"
+	"overwatch.jpg"
+	"pubg.jpg"
+	"rainbowsix.jpg"
+	"rl.jpg"
+	"warzone.jpg"
+];
+
+var names = [
+	"Among us",
+	"Apex Legends",
+	"CS:GO",
+	"Fortnite",
+	"PubG",
+	"Rainbow Six Siege",
+	"Call of Duty Warzone",
+	"Rocket League",
+	"League of Legends",
+	"Grand Theft Auto 5",
+	"Minecraft",
+	"Overwatch"
+];
