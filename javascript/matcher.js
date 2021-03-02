77194a748033fe4e64478e98972919a3bd1f6bf3
Nick <Nickjv2000@hotmail.nl>
2021-03-02 13:33:33 +0100

    Changes matcher.js
    Removed the arrays, changed the variable of random to randoms, added consts for onclicks. changed start function for arrays

--- javascript/matcher.js
+++ javascript/matcher.js
@@ -1,69 +1,37 @@
-
-var games = [
-	{name:"Among us ", image: "<img src='../images/among.jpg' style='width:353px;height:200px;' alt='among us'>"},
-	{name:"Apex ",image: "<img src='../images/apex.jpg' style='width:353px;height:200px;' alt='apex legends'>"},
-	{name:"CS:GO ",image: "<img src='../images/csgo.jpg' style='width:353px;height:200px;' alt='csgo'>"},
-	{name:"Fortnite ",image: "<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
-	{name:"GTA ",image: "<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
-	{name:"LoL ",image: "<img src='../images/LoL.jpg' style='width:353px;height:200px;' alt='league of legends'>"},
-	{name:"MC ",image: "<img src='../images/minecraft.jpg' style='width:353px;height:200px;' alt='minecraft'>"},
-	{name:"OW ",image: "<img src='../images/overwatch.jpg' style='width:353px;height:200px;' alt='overwatch'>"},
-	{name:"PUBG",image: "<img src='../images/pubg.jpg' style='width:353px;height:200px;' alt='pubg'>"},
-	{name:"R6 ",image: "<img src='../images/rainbowsix.jpg' style='width:353px;height:200px;' alt='rainbow six siege'>"},
-	{name:"RL ",image: "<img src='../images/rl.jpg' style='width:353px;height:200px;' alt='rocket league'>"},
-	{name:"Warzone ",image: "<img src='../images/warzone.jpg' style='width:353px;height:200px;' alt='warzone'>"}
-];
-
-var ids = [
-	"but",
-	"but1",
-	"but2",
-	"but3",
-	"but4",
-	"but5",
-	"but6",
-	"but7",
-	"but8",
-	"but9",
-	"but10",
-	"but11"
-];
-
-var imgs = [
-	"img",
-	"img1",
-	"img2",
-	"img3",
-	"img4",
-	"img5",
-	"img6",
-	"img7",
-	"img8",
-	"img9",
-	"img10",
-	"img11"
-];
-
 // Aanmaken van variabele waarin een random nummer word gepakt
 // id variabele verkort document.getelementbyid naar id
 // test linked een naam van array names en array images
 
 var id = function(id) {return document.getElementById(id);};
-var random = Math.floor(Math.random() * games.length);
+var randoms = Math.floor(Math.random() * games.length);
 var score = 0;
 
 // een log om te checken wat de uitkomst van de variable zijn
 
 console.log(score);
 
-// start de matching game en geeft alles een naam & img en maakt alles visible
+// zorgt ervoor dat elke but id word opgepakt
 
-function Start() {
+const start = id("bt1");
+const reset = id("bt2");
+const answer = document.getElementsByClassName("btn");
+
+console.log(answer);
+// start de matching game
+// geeft alles een naam & img
+// maakt alles visible
+// maakt een random nummer
+
+function generateNumber() {
+	return Math.floor(Math.random() * games.length);
+}
+
+start.onclick = function start() {
 	for (i = 0; i < games.length; i++) {	
- 	 	id(ids[i]).innerHTML = games[i].name;
- 	 	id(imgs[i]).innerHTML = games[i].image;
- 	 	id(ids[i]).style.visibility = "visible";
-		id(imgs[i]).style.visibility = "visible";
+	 	id(ids[i].id).innerHTML = games[generateNumber()].name;
+ 		id(ids[i].img).innerHTML = games[generateNumber()].image;
+ 		id(ids[i].id).style.visibility = "visible";
+		id(ids[i].img).style.visibility = "visible";
 	} 
 	id("dropdown").style.visibility = "visible";
 	id("text1").style.visibility = "visible";
@@ -73,17 +41,20 @@ function Start() {
 }
 
 // als je op een button klikt dan checkt de functie of de img en naam gelijk zijn
-// en geeft dan een verandering op de variabele score
-
-function Guess() {
-		if(ids == imgs) {
-			score = score + 1;
-		} else {
+// geeft dan een verandering op de variabele score
+ 
+answer.onclick = function guess() {
+	if(games.name == games.image) {
+		score = score + 1;
+	} else if(games.name != games.image) {
 		score = score - 1;
-		}
-		console.log(score);
-}
+	}
+	console.log(score);
+};
 
-function Reset() {
+// reload de webpagina wanneer je op bt2 drukt
+
+reset.onclick = function reset() {
 	location.reload();
 }
+
