5517ee81b6ba67c80d1bda9bb0c80c6640d49da8
Nick <Nickjv2000@hotmail.nl>
2021-03-02 11:07:38 +0100

    changes to arrays, matcher and frontpage
    changed the way of array and same as in the code

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,59 +1,30 @@
-var images = [
-	"<img src='../images/among.jpg' style='width:353px;height:200px;' alt='among us'>",
-	"<img src='../images/apex.jpg' style='width:353px;height:200px;' alt='apex legends'>",
-	"<img src='../images/csgo.jpg' style='width:353px;height:200px;' alt='csgo'>",
-	"<img src='../images/fortnite.jpg' style='width:353px;height:200px;' alt='fortnite'>",
-	"<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>",
-	"<img src='../images/LoL.jpg' style='width:353px;height:200px;' alt='league of legends'>",
-	"<img src='../images/minecraft.jpg' style='width:353px;height:200px;' alt='minecraft'>",
-	"<img src='../images/overwatch.jpg' style='width:353px;height:200px;' alt='overwach'>",
-	"<img src='../images/pubg.jpg' style='width:353px;height:200px;' alt='pubg'>",
-	"<img src='../images/rainbowsix.jpg' style='width:353px;height:200px;' alt='rainbow six siege'>",
-	"<img src='../images/rl.jpg' style='width:353px;height:200px;' alt='rocket league'>",
-	"<img src='../images/warzone.jpg' style='width:353px;height:200px;' alt='warzone'>"
-];
-
-var names = [
-	"Among us ",
-	"Apex ",
-	"CS:GO ",
-	"Fortnite ",
-	"GTA ",
-	"LoL ",
-	"MC ",
-	"Overwatch ",
-	"PUBG ",
-	"R6 ",
-	"RL ",
-	"Warzone "
+var games = [
+	{name:"Among us ", image:"<img src='../images/among.jpg' style='width:353px;height:200px;' alt='among us'>"},
+	{name:"Apex ",image:"<img src='../images/apex.jpg' style='width:353px;height:200px;' alt='apex legends'>"},
+	{name:"CS:GO ",image:"<img src='../images/csgo.jpg' style='width:353px;height:200px;' alt='csgo'>"},
+	{name:"Fortnite ",image:"<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
+	{name:"GTA ",image:"<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
+	{name:"LoL ",image:"<img src='../images/LoL.jpg' style='width:353px;height:200px;' alt='league of legends'>"},
+	{name:"MC ",image:"<img src='../images/minecraft.jpg' style='width:353px;height:200px;' alt='minecraft'>"},
+	{name:"OW ",image:"<img src='../images/overwatch.jpg' style='width:353px;height:200px;' alt='overwatch'>"},
+	{name:"PUBG",image:"<img src='../images/pubg.jpg' style='width:353px;height:200px;' alt='pubg'>"},
+	{name:"R6 ",image:"<img src='../images/rainbowsix.jpg' style='width:353px;height:200px;' alt='rainbow six siege'>"},
+	{name:"RL ",image:"<img src='../images/rl.jpg' style='width:353px;height:200px;' alt='rocket league'>"},
+	{name:"Warzone ",image:"<img src='../images/warzone.jpg' style='width:353px;height:200px;' alt='warzone'>"}
 ];
 
 var ids = [
-	"but",
-	"but1",
-	"but2",
-	"but3",
-	"but4",
-	"but5",
-	"but6",
-	"but7",
-	"but8",
-	"but9",
-	"but10",
-	"but11"
+	{id:"but", img:"img1"}
+	{"but1", img:"img1"}
+	{"but2", img:"img1"}
+	{"but3", img:"img1"}
+	{"but4", img:"img1"}
+	{"but5", img:"img1"}
+	{"but6", img:"img1"}
+	{"but7", img:"img1"}
+	{"but8", img:"img1"}
+	{"but9", img:"img1"}
+	{"but10", img:"img1"}
+	{"but11", img:"img1"}
 ];
 
-var imgs = [
-	"img1",
-	"img2",
-	"img3",
-	"img4",
-	"img5",
-	"img6",
-	"img7",
-	"img8",
-	"img9",
-	"img10",
-	"img11",
-	"img12"
-];
--- javascript/matcher.js
+++ javascript/matcher.js
@@ -1,31 +1,17 @@
-var images = [
-	"<img src='../images/among.jpg' style='width:353px;height:200px;' alt='among us'>",
-	"<img src='../images/apex.jpg' style='width:353px;height:200px;' alt='apex legends'>",
-	"<img src='../images/csgo.jpg' style='width:353px;height:200px;' alt='csgo'>",
-	"<img src='../images/fortnite.jpg' style='width:353px;height:200px;' alt='fortnite'>",
-	"<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>",
-	"<img src='../images/LoL.jpg' style='width:353px;height:200px;' alt='league of legends'>",
-	"<img src='../images/minecraft.jpg' style='width:353px;height:200px;' alt='minecraft'>",
-	"<img src='../images/overwatch.jpg' style='width:353px;height:200px;' alt='overwach'>",
-	"<img src='../images/pubg.jpg' style='width:353px;height:200px;' alt='pubg'>",
-	"<img src='../images/rainbowsix.jpg' style='width:353px;height:200px;' alt='rainbow six siege'>",
-	"<img src='../images/rl.jpg' style='width:353px;height:200px;' alt='rocket league'>",
-	"<img src='../images/warzone.jpg' style='width:353px;height:200px;' alt='warzone'>"
-];
 
-var names = [
-	"Among us ",
-	"Apex ",
-	"CS:GO ",
-	"Fortnite ",
-	"GTA ",
-	"LoL ",
-	"MC ",
-	"Overwatch ",
-	"PUBG ",
-	"R6 ",
-	"RL ",
-	"Warzone "
+var games = [
+	{name:"Among us ", image: "<img src='../images/among.jpg' style='width:353px;height:200px;' alt='among us'>"},
+	{name:"Apex ",image: "<img src='../images/apex.jpg' style='width:353px;height:200px;' alt='apex legends'>"},
+	{name:"CS:GO ",image: "<img src='../images/csgo.jpg' style='width:353px;height:200px;' alt='csgo'>"},
+	{name:"Fortnite ",image: "<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
+	{name:"GTA ",image: "<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
+	{name:"LoL ",image: "<img src='../images/LoL.jpg' style='width:353px;height:200px;' alt='league of legends'>"},
+	{name:"MC ",image: "<img src='../images/minecraft.jpg' style='width:353px;height:200px;' alt='minecraft'>"},
+	{name:"OW ",image: "<img src='../images/overwatch.jpg' style='width:353px;height:200px;' alt='overwatch'>"},
+	{name:"PUBG",image: "<img src='../images/pubg.jpg' style='width:353px;height:200px;' alt='pubg'>"},
+	{name:"R6 ",image: "<img src='../images/rainbowsix.jpg' style='width:353px;height:200px;' alt='rainbow six siege'>"},
+	{name:"RL ",image: "<img src='../images/rl.jpg' style='width:353px;height:200px;' alt='rocket league'>"},
+	{name:"Warzone ",image: "<img src='../images/warzone.jpg' style='width:353px;height:200px;' alt='warzone'>"}
 ];
 
 var ids = [
@@ -63,21 +49,19 @@ var imgs = [
 // test linked een naam van array names en array images
 
 var id = function(id) {return document.getElementById(id);};
-var random = Math.floor(Math.random() * images.length);
-var test = names[random] + images[random];
+var random = Math.floor(Math.random() * games.length);
 var score = 0;
 
 // een log om te checken wat de uitkomst van de variable zijn
 
-console.log(test);
 console.log(score);
 
 // start de matching game en geeft alles een naam & img en maakt alles visible
 
 function Start() {
-	for (i = 0; i < names.length; i++) {	
- 	 	id(ids[i]).innerHTML = names[i];
- 	 	id(imgs[i]).innerHTML = images[i];
+	for (i = 0; i < games.length; i++) {	
+ 	 	id(ids[i]).innerHTML = games[i].name;
+ 	 	id(imgs[i]).innerHTML = games[i].image;
  	 	id(ids[i]).style.visibility = "visible";
 		id(imgs[i]).style.visibility = "visible";
 	} 
