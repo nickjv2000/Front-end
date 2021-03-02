7d5ef50f8412e33e3add5aa8c9400e89c75ddea9
Nick <Nickjv2000@hotmail.nl>
2021-03-02 09:38:02 +0100

    Hiding all files before pressing Start button

---
+++ javascript/prestart.js
@@ -0,0 +1,33 @@
+// verkort document.getelementbyid naar id
+
+var id = function(id) {return document.getElementById(id);};
+
+// voordat er op start word gedrukt word alles gehide door middel van deze code
+
+id("dropdown").style.visibility = "hidden";
+id("text1").style.visibility = "hidden";
+id("but").style.visibility = "hidden";
+id("but1").style.visibility = "hidden";
+id("but2").style.visibility = "hidden";
+id("but3").style.visibility = "hidden";
+id("but4").style.visibility = "hidden";
+id("but5").style.visibility = "hidden";
+id("but6").style.visibility = "hidden";
+id("but7").style.visibility = "hidden";
+id("but8").style.visibility = "hidden";
+id("but9").style.visibility = "hidden";
+id("but10").style.visibility = "hidden";
+id("but11").style.visibility = "hidden";
+
+id("img").style.visibility = "hidden";
+id("img1").style.visibility = "hidden";
+id("img2").style.visibility = "hidden";
+id("img3").style.visibility = "hidden";
+id("img4").style.visibility = "hidden";
+id("img5").style.visibility = "hidden";
+id("img6").style.visibility = "hidden";
+id("img7").style.visibility = "hidden";
+id("img8").style.visibility = "hidden";
+id("img9").style.visibility = "hidden";
+id("img10").style.visibility = "hidden";
+id("img11").style.visibility = "hidden";
