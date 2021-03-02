fe38495cda6b8cf51638f8a708d73126216a637b
Nick <Nickjv2000@hotmail.nl>
2021-03-02 10:39:23 +0100

    Bt2
    Made it so that bt2 only shows when playing and not beforehand

--- javascript/prestart.js
+++ javascript/prestart.js
@@ -1,31 +1,33 @@
 
 
-document.getElementById("dropdown").style.visibility = "hidden";
+id("dropdown").style.visibility = "hidden";
 
-document.getElementById("text1").style.visibility = "hidden";
+id("text1").style.visibility = "hidden";
+id("text2").style.visibility = "hidden";
+id("bt2").style.visibility = "hidden";
 
-document.getElementById("but").style.visibility = "hidden";
-document.getElementById("but1").style.visibility = "hidden";
-document.getElementById("but2").style.visibility = "hidden";
-document.getElementById("but3").style.visibility = "hidden";
-document.getElementById("but4").style.visibility = "hidden";
-document.getElementById("but5").style.visibility = "hidden";
-document.getElementById("but6").style.visibility = "hidden";
-document.getElementById("but7").style.visibility = "hidden";
-document.getElementById("but8").style.visibility = "hidden";
-document.getElementById("but9").style.visibility = "hidden";
-document.getElementById("but10").style.visibility = "hidden";
-document.getElementById("but11").style.visibility = "hidden";
+id("but").style.visibility = "hidden";
+id("but1").style.visibility = "hidden";
+id("but2").style.visibility = "hidden";
+id("but3").style.visibility = "hidden";
+id("but4").style.visibility = "hidden";
+id("but5").style.visibility = "hidden";
+id("but6").style.visibility = "hidden";
+id("but7").style.visibility = "hidden";
+id("but8").style.visibility = "hidden";
+id("but9").style.visibility = "hidden";
+id("but10").style.visibility = "hidden";
+id("but11").style.visibility = "hidden";
 
-document.getElementById("img").style.visibility = "hidden";
-document.getElementById("img1").style.visibility = "hidden";
-document.getElementById("img2").style.visibility = "hidden";
-document.getElementById("img3").style.visibility = "hidden";
-document.getElementById("img4").style.visibility = "hidden";
-document.getElementById("img5").style.visibility = "hidden";
-document.getElementById("img6").style.visibility = "hidden";
-document.getElementById("img7").style.visibility = "hidden";
-document.getElementById("img8").style.visibility = "hidden";
-document.getElementById("img9").style.visibility = "hidden";
-document.getElementById("img10").style.visibility = "hidden";
-document.getElementById("img11").style.visibility = "hidden";
+id("img").style.visibility = "hidden";
+id("img1").style.visibility = "hidden";
+id("img2").style.visibility = "hidden";
+id("img3").style.visibility = "hidden";
+id("img4").style.visibility = "hidden";
+id("img5").style.visibility = "hidden";
+id("img6").style.visibility = "hidden";
+id("img7").style.visibility = "hidden";
+id("img8").style.visibility = "hidden";
+id("img9").style.visibility = "hidden";
+id("img10").style.visibility = "hidden";
+id("img11").style.visibility = "hidden";
