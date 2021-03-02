5235265494c8a34fe62aeeae6845e23d8609f8df
Nick <Nickjv2000@hotmail.nl>
2021-03-02 09:39:03 +0100

    Array file (error)
    Put all arrays into 1 file (doesnt work)

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,29 +1,59 @@
 var images = [
-	"among.jpg"
-	"apex.jpg"
-	"csgo.jpg"
-	"fortnite.jpg"
-	"GTA.jpg"
-	"LoL.jpg"
-	"minecraft.jpg"
-	"overwatch.jpg"
-	"pubg.jpg"
-	"rainbowsix.jpg"
-	"rl.jpg"
-	"warzone.jpg"
+	"<img src='../images/among.jpg' style='width:353px;height:200px;' alt='among us'>",
+	"<img src='../images/apex.jpg' style='width:353px;height:200px;' alt='apex legends'>",
+	"<img src='../images/csgo.jpg' style='width:353px;height:200px;' alt='csgo'>",
+	"<img src='../images/fortnite.jpg' style='width:353px;height:200px;' alt='fortnite'>",
+	"<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>",
+	"<img src='../images/LoL.jpg' style='width:353px;height:200px;' alt='league of legends'>",
+	"<img src='../images/minecraft.jpg' style='width:353px;height:200px;' alt='minecraft'>",
+	"<img src='../images/overwatch.jpg' style='width:353px;height:200px;' alt='overwach'>",
+	"<img src='../images/pubg.jpg' style='width:353px;height:200px;' alt='pubg'>",
+	"<img src='../images/rainbowsix.jpg' style='width:353px;height:200px;' alt='rainbow six siege'>",
+	"<img src='../images/rl.jpg' style='width:353px;height:200px;' alt='rocket league'>",
+	"<img src='../images/warzone.jpg' style='width:353px;height:200px;' alt='warzone'>"
 ];
 
 var names = [
-	"Among us",
-	"Apex Legends",
-	"CS:GO",
-	"Fortnite",
-	"PubG",
-	"Rainbow Six Siege",
-	"Call of Duty Warzone",
-	"Rocket League",
-	"League of Legends",
-	"Grand Theft Auto 5",
-	"Minecraft",
-	"Overwatch"
+	"Among us ",
+	"Apex ",
+	"CS:GO ",
+	"Fortnite ",
+	"GTA ",
+	"LoL ",
+	"MC ",
+	"Overwatch ",
+	"PUBG ",
+	"R6 ",
+	"RL ",
+	"Warzone "
+];
+
+var ids = [
+	"but",
+	"but1",
+	"but2",
+	"but3",
+	"but4",
+	"but5",
+	"but6",
+	"but7",
+	"but8",
+	"but9",
+	"but10",
+	"but11"
+];
+
+var imgs = [
+	"img1",
+	"img2",
+	"img3",
+	"img4",
+	"img5",
+	"img6",
+	"img7",
+	"img8",
+	"img9",
+	"img10",
+	"img11",
+	"img12"
 ];
