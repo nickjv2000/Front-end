cc15a70fcee26861dbc7831120f262490fed9ea4
Nick <Nickjv2000@hotmail.nl>
2021-03-03 10:30:20 +0100

    Prestart.js comments
    Comments toevoegen op prestart.js

--- javascript/prestart.js
+++ javascript/prestart.js
@@ -1,4 +1,8 @@
+// document.getelementbyid verkorten naar id
 var id = function(id) {return document.getElementById(id);};
+
+// alles wat niet de start button is, 
+// hiden aan het begin zodat het opgeroepen kan worden daarna
 
 id("dropdown").style.visibility = "hidden";
 
