75cb45087d6d3fce62526b214bf34571b3145600
Nick <Nickjv2000@hotmail.nl>
2021-03-23 11:23:28 +0100

    JS
    Changes for arrays (smaller) and removal of unused visibility code

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,29 +1,14 @@
 var games = [
-	{name:"Among us ", image:"<img src='../images/among.jpg' style='width:353px;height:200px;' alt='among us'>"},
-	{name:"Apex ",image:"<img src='../images/apex.jpg' style='width:353px;height:200px;' alt='apex legends'>"},
-	{name:"CS:GO ",image:"<img src='../images/csgo.jpg' style='width:353px;height:200px;' alt='csgo'>"},
-	{name:"Fortnite ",image:"<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
-	{name:"GTA ",image:"<img src='../images/GTA.jpg' style='width:353px;height:200px;' alt='grand theft auto'>"},
-	{name:"LoL ",image:"<img src='../images/LoL.jpg' style='width:353px;height:200px;' alt='league of legends'>"},
-	{name:"MC ",image:"<img src='../images/minecraft.jpg' style='width:353px;height:200px;' alt='minecraft'>"},
-	{name:"OW ",image:"<img src='../images/overwatch.jpg' style='width:353px;height:200px;' alt='overwatch'>"},
-	{name:"PUBG",image:"<img src='../images/pubg.jpg' style='width:353px;height:200px;' alt='pubg'>"},
-	{name:"R6 ",image:"<img src='../images/rainbowsix.jpg' style='width:353px;height:200px;' alt='rainbow six siege'>"},
-	{name:"RL ",image:"<img src='../images/rl.jpg' style='width:353px;height:200px;' alt='rocket league'>"},
-	{name:"Warzone ",image:"<img src='../images/warzone.jpg' style='width:353px;height:200px;' alt='warzone'>"}
-];
-
-var ids = [
-	{id:"but", img:"img"},
-	{id:"but1", img:"img1"},
-	{id:"but2", img:"img2"},
-	{id:"but3", img:"img3"},
-	{id:"but4", img:"img4"},
-	{id:"but5", img:"img5"},
-	{id:"but6", img:"img6"},
-	{id:"but7", img:"img7"},
-	{id:"but8", img:"img8"},
-	{id:"but9", img:"img9"},
-	{id:"but10", img:"img10"},
-	{id:"but11", img:"img11"}
+	{name:"Among us ", 	image:"../images/among.jpg"},
+	{name:"Apex ",		image:"../images/apex.jpg"},
+	{name:"CS:GO ",		image:"../images/csgo.jpg"},
+	{name:"Fortnite ",	image:"../images/GTA.jpg"},
+	{name:"GTA ",		image:"../images/GTA.jpg"},
+	{name:"LoL ",		image:"../images/LoL.jpg"},
+	{name:"MC ",		image:"../images/minecraft.jpg"},
+	{name:"OW ",		image:"../images/overwatch.jpg"},
+	{name:"PUBG",		image:"../images/pubg.jpg"},
+	{name:"R6 ",		image:"../images/rainbowsix.jpg"},
+	{name:"RL ",		image:"../images/rl.jpg"},
+	{name:"Warzone ",	image:"../images/warzone.jpg"}
 ];
