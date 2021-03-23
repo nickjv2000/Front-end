4a811796f4a45413d18ba5de6697eaa797a714a3
Nick <Nickjv2000@hotmail.nl>
2021-03-23 11:23:06 +0100

    JS & HTML
    Smaller code

--- javascript/matcher.js
+++ javascript/matcher.js
@@ -5,6 +5,7 @@
 var id = function(id) {return document.getElementById(id);};
 var randoms = Math.floor(Math.random() * games.length);
 var score = 0;
+var div = document.createElement('div');
 
 // een log om te checken wat de uitkomst van de variable zijn
 
@@ -17,8 +18,7 @@ const start = id("bt1");
 const reset = id("bt2");
 const answers = document.getElementsByClassName("#but1");
 
-console.log(answer);
-
+console.log(answers);
 
 // maakt een random nummer
 
@@ -26,17 +26,9 @@ function generateNumber() {
 	return Math.floor(Math.random() * games.length);
 }
 
-// start de matching game
-// geeft alles een naam & img
-// maakt alles visible
+// zorgt ervoor dat de elementen tevoorschijn komen
 
-start.onclick = function start() {
-	for (i = 0; i < games.length; i++) {	
-	 	id(ids[i].id).innerHTML = games[generateNumber()].name;
- 		id(ids[i].img).innerHTML = games[generateNumber()].image;
- 		id(ids[i].id).style.visibility = "visible";
-		id(ids[i].img).style.visibility = "visible";
-	} 
+function visibleElements() {
 	id("dropdown").style.visibility = "visible";
 	id("text1").style.visibility = "visible";
 	id("text2").style.visibility = "visible";
@@ -44,17 +36,46 @@ start.onclick = function start() {
 	id("bt2").style.visibility = "visible";
 }
 
+// start de matching game
+// geeft alles een naam & img
+// maakt alles visible
+
+start.onclick = function start() {
+
+	visibleElements();
+
+	var containerDiv = document.getElementById("containers");
+
+	for (u = 0; u < games.length; u++) {
+
+		var div1 = document.createElement("div");
+		div1.className = "card";
+
+		var div2 = document.createElement("img");
+		div2.className = "img";
+		div2.src = games[generateNumber()].img;
+
+		div1.appendChild(div2);
+
+		var btn = document.createElement("button");
+		btn.className = "btn btn-dark";
+		div1.appendChild(btn);
+
+		containerDiv.appendChild(div1);
+	}	
+}
+
 // als je op een button klikt dan checkt de functie of de img en naam gelijk zijn
 // geeft dan een verandering op de variabele score
  
-answers.onclick = function guess() {
-	if(games.name == games.image) {
-		score = score + 1;
-	} else if(games.name != games.image) {
-		score = score - 1;
-	}
-	console.log(score);
-};
+// answers.onclick = function guess() {
+// 	if(games.name == games.image) {
+// 		score = score + 1;
+// 	} else if(games.name != games.image) {
+// 		score = score - 1;
+// 	}
+// 	console.log(score);
+// };
 
 // reload de webpagina wanneer je op bt2 drukt
 
