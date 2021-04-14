14d24b2db07fac144d799ad537a43ccf90807779
Nick <Nickjv2000@hotmail.nl>
2021-04-14 11:09:49 +0200

    Main JS & images & html
    Changed var score from 0 to 10
    Added a comment
    Added a const for bt3
    Made a finish button
    Adjusted the visibility for the button
    Added a finish function
    Fixed matching system 
    Added comments to the finish function
    Added images for the result score screen
    Minor css changes for the text
    Added 4 texts for result screen

--- javascript/matcher.js
+++ javascript/matcher.js
@@ -3,14 +3,17 @@
 
 var id = function(id) {return document.getElementById(id);};
 var randoms = Math.floor(Math.random() * games.length);
-var score = 0;
+var score = 10;
 var div = document.createElement('div');
 
 // giving id's bt1 and bt2 a const to link with a function
+// hides finish button
 
 const start = id("bt1");
 const reset = id("bt2");
+const finish = id("bt3");
 
+finish.style.visibility = "hidden";
 // Creates random numbers
 
 function generateNumber() {
@@ -23,8 +26,13 @@ function visibleElements() {
 	id("dropdown").style.visibility = "visible";
 	id("text1").style.visibility = "visible";
 	id("text2").style.visibility = "visible";
-	id("bt1").style.visibility = "hidden";
-	id("bt2").style.visibility = "visible";
+	reset.style.visibility = "visible";
+	finish.style.visibility = "visible";
+	start.remove();
+	id("text3").style.display = "none";
+	id("text4").style.display = "none";
+	id("text5").style.display = "none";
+	id("text6").style.display = "none";
 }
 
 // Start the matching game
@@ -43,11 +51,11 @@ start.onclick = function start() {
 
 	for (u = 0; u < games.length; u++) {
 
-	randomImage = generateNumber()
-	randomName = generateNumber()
+	randomImage = generateNumber();
+	randomName = generateNumber();
 
-	console.log(randomImage);
-	console.log(randomName);
+	console.log(games[randomName].name + ": " + randomImage + " " + randomName);
+	// console.log(randomName);
 	
 	var div1 = document.createElement("div");
 	div1.className = "card";
@@ -63,19 +71,71 @@ start.onclick = function start() {
 	btn.innerHTML = games[randomName].name;
 
 	if(randomName == randomImage) {
-		btn.onclick = score + 1;
+		btn.onclick = function(){score += 1};
 	} else if(randomName != randomImage) {
-		btn.onclick = score - 1;
+		btn.onclick = function(){score -= 1};
 	}
 
 	div1.appendChild(btn);
 
 	div0.appendChild(div1);
 	containerDiv.appendChild(div0);
-	}	
 
-	console.log(score);
-	
+	}	
+}
+
+// A function that loads the last page of the website
+// showing the results of the game
+// removes any buttons/text that was made prior to the result screen
+// adds back the text 3 up to 6 for results
+// looks at what number the score is, depending on the score gives text
+
+finish.onclick = function finish() {
+	id("dropdown").remove();
+	id("text1").remove();
+	id("text2").remove();
+	id("containers").remove();
+	id("bt3").remove();
+
+	reset.remove();
+
+	id("text3").style.display = "";
+	id("text4").style.display = "";
+	id("text5").style.display = "";
+	id("text6").style.display = "";
+
+	var result = document.getElementById("text3");
+	result.innerHTML = "Your score is: " + score;
+
+	if (score > 10){
+		document.body.style.backgroundImage = "url('../images/happy.gif')";
+		var resultTexts = document.getElementById("text4");
+		resultTexts.innerHTML = "If you've gotten a score above 10, congratulations you've done very well!"
+		id("text3").style.color = "red";
+		id("text4").style.color = "red";
+		id("text5").style.color = "red";
+		id("text6").style.color = "red";
+		id("text3").style.fontSize = "x-large";
+		id("text4").style.fontSize = "x-large";
+		id("text5").style.fontSize = "x-large";
+		id("text6").style.fontSize = "x-large";
+	} else if (score < 10) {
+		document.body.style.backgroundImage = "url('../images/sad.png')";
+		var resultTextz = document.getElementById("text5");
+		resultTextz.innerHTML = "Is the score below 10? You've done poorly."
+		id("text3").style.fontSize = "x-large";
+		id("text4").style.fontSize = "x-large";
+		id("text5").style.fontSize = "x-large";
+		id("text6").style.fontSize = "x-large";
+	} else if (score == 10) {
+		document.body.style.backgroundImage = "url('../images/neutral.png')";
+		var resultTexta = document.getElementById("text6");
+		resultTexta.innerHTML = "Is the score 10 on the dot? Well you could've done nothing else to change that."
+		id("text3").style.fontSize = "x-large";
+		id("text4").style.fontSize = "x-large";
+		id("text5").style.fontSize = "x-large";
+		id("text6").style.fontSize = "x-large";
+	}
 }
 
 // Reload the page when pressing the "Reset" button
