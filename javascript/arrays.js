30d918a359f8baf435ec29fc00e21cd9a2e0536e
Nick <Nickjv2000@hotmail.nl>
2021-12-03 10:20:32 +0100

    Array JS
    placeholder for audio

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,14 +1,14 @@
 var games = [
-	{name:"Among us ", 	image:"../images/among.jpg",      audio:"../images/amongus.mp3"},
+	{name:"Among us ", 	image:"../images/among.jpg",      audio:"../images/csgo.mp3"},
 	{name:"Apex ",		image:"../images/apex.jpg",       audio:"../images/apex.mp3"},
 	{name:"CS:GO ",		image:"../images/csgo.jpg",       audio:"../images/csgo.mp3"},
 	{name:"Fortnite ",	image:"../images/fortnite.jpg",   audio:"../images/fortnite.mp3"},
-	{name:"GTA ",		image:"../images/GTA.jpg",        audio:"../images/gta.mp3"},
+	{name:"GTA ",		image:"../images/GTA.jpg",        audio:"../images/csgo.mp3"},
 	{name:"LoL ",		image:"../images/LoL.jpg",        audio:"../images/league.mp3"},
 	{name:"MC ",		image:"../images/minecraft.jpg",  audio:"../images/mc.mp3"},
-	{name:"OW ",		image:"../images/overwatch.jpg",  audio:"../images/ow.mp3"},
+	{name:"OW ",		image:"../images/overwatch.jpg",  audio:"../images/csgo.mp3"},
 	{name:"PUBG",		image:"../images/pubg.jpg",       audio:"../images/pubg.mp3"},
-	{name:"R6 ",		image:"../images/rainbowsix.jpg", audio:"../images/rainbow.mp3"},
+	{name:"R6 ",		image:"../images/rainbowsix.jpg", audio:"../images/csgo.mp3"},
 	{name:"RL ",		image:"../images/rl.jpg",         audio:"../images/rl.mp3"},
 	{name:"Warzone ",	image:"../images/warzone.jpg",    audio:"../images/warzone.mp3"}
 ];
